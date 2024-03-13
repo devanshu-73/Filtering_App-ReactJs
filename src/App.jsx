24273eeec556557280732460b63b274ed52ddf1f
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-03-13 20:13:07 +0530

    Filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import Login from './Comps/Login';
+import Filter from './Pages/Filter';
 
 const App = () => {
   return (
     <>
       {/* <Login /> */}
+      <Filter />
     </>
   );
 };
--- src/Pages/Filter.jsx
+++ src/Pages/Filter.jsx
@@ -1,14 +1,42 @@
+import { useState } from "react"
+import axios from 'axios';
 
 const Filter = () => {
-    const handleOnchange =()=>{
-        console.log("Hello");
+    const [Items, setItems] = useState([]);
+    // useEffect(() => {
+    //     handleSubmit()
+    // }, [])
+    // const handleOnchange = () => {
+
+    // }
+    const handleSubmit = async () => {
+        const res = await axios.get('http://localhost:3001/filter')
+        setItems(res.data)
+        console.log(res.data);
     }
     return (
-    <>
-     <input type="search" name="search" id="search" onChange={handleOnchange} placeholder="Search Here..." />   
-
-    </>
-  )
+        <>
+            {/* <input type="search" name="search" id="search" onChange={handleOnchange} placeholder="Search Here..." /> */}
+            <input type="submit" onClick={handleSubmit} value="Submit" />
+            <hr />
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Items.map((item) => {
+                        <tr>
+                            <td>{item.id}</td>
+                            <td>{item.title}</td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </>
+    )
 }
 
 export default Filter

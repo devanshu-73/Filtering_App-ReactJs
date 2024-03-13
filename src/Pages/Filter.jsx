c960f80e91e28eceba1cc499cfb95d723c952b75
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-03-13 20:21:25 +0530

    Filter

--- src/Pages/Filter.jsx
+++ src/Pages/Filter.jsx
@@ -1,25 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from 'axios';
-
 const Filter = () => {
     const [Items, setItems] = useState([]);
-    // useEffect(() => {
-    //     handleSubmit()
-    // }, [])
-    // const handleOnchange = () => {
-
-    // }
+    const [Value, setValue] = useState();
+    useEffect(() => {
+        handleSubmit()
+    }, [Value])
+    const handleOnchange = (e) => {
+        setValue(e.target.value)
+        console.log(Value);
+    }
     const handleSubmit = async () => {
-        const res = await axios.get('http://localhost:3001/filter')
+        
+        let res = await axios.get('http://localhost:3001/filter')
         setItems(res.data)
         console.log(res.data);
     }
     return (
         <>
-            {/* <input type="search" name="search" id="search" onChange={handleOnchange} placeholder="Search Here..." /> */}
+            <input type="search" name="search" id="search" onChange={handleOnchange} value={Value} placeholder="Search Here..." />
             <input type="submit" onClick={handleSubmit} value="Submit" />
             <hr />
-            <table>
+            <table border={1}>
                 <thead>
                     <tr>
                         <th>Id</th>
@@ -27,11 +29,13 @@ const Filter = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {Items.map((item) => {
-                        <tr>
-                            <td>{item.id}</td>
-                            <td>{item.title}</td>
-                        </tr>
+
+                    {Items.map((item, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{item.id}</td>
+                                <td>{item.title}</td>
+                            </tr>)
                     })}
                 </tbody>
             </table>
